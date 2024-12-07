8fe92a3743431edb37811a68decfa6f2e09d49e1
Michael Todd <eltodd@gmail.com>
2024-12-07 11:58:43 -0500

    cont

--- include/yeastcpp/data_structures/motion_command.hpp
+++ include/yeastcpp/data_structures/motion_command.hpp
@@ -16,5 +16,7 @@ namespace yeast_motion
         Twist2D acceleration;
 
         MotionCommand(nlohmann::json json);
+        nlohmann::json to_json ();
+        void from_json(nlohmann::json json);
     };
 }
