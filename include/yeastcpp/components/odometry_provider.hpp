97ccb8d85b2f53f354c1f206f63b00060ebff0a2
Michael Todd <eltodd@gmail.com>
2025-02-08 16:00:22 -0500

    Updated to support vision fusion

--- include/yeastcpp/components/odometry_provider.hpp
+++ include/yeastcpp/components/odometry_provider.hpp
@@ -5,6 +5,7 @@
 #include "yeastcpp/data_structures/rotation2d.hpp"
 #include "yeastcpp/data_structures/odometry_sample.hpp"
 #include "yeastcpp/data_structures/swerve_module_status.hpp"
+#include "yeastcpp/data_structures/absolute_pose_estimate.hpp"
 
 namespace yeast_motion
 {
@@ -13,5 +14,6 @@ namespace yeast_motion
         public:
         virtual OdometrySample update (std::vector<SwerveModuleStatus> status, Rotation2D gyro_angle) = 0;
         virtual OdometrySample reset (OdometrySample reset_sample) = 0;
+        virtual void provide_absolute_position_estimate (AbsolutePoseEstimate estimate) = 0;
     };
 }
